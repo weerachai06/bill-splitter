65b132ba671b4e57fda6234f8e7ec2ccc9e0caa1
PetchDev <clkeen157@gmail.com>
2025-12-22 08:19:28 +0700

    feat: add streaming response endpoints for delayed and static data

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,12 +1,41 @@
+use std::time::Duration;
+
 use actix_web::{
-    get, http::StatusCode, middleware::ErrorHandlers, web, App, HttpResponse, HttpServer,
+    get, http::StatusCode, middleware::ErrorHandlers, web, App, Error, HttpResponse, HttpServer,
     Responder, Result,
 };
+
+use futures::{future::ok, stream::once};
 use serde_json::json;
 
 #[get("/hello/{name}")]
 async fn greet(name: web::Path<String>) -> impl Responder {
     format!("Hello {name}!")
+}
+
+#[get("/stream-delay")]
+async fn stream_delay() -> HttpResponse {
+    let tick_duration = Duration::from_millis(10);
+
+    let body = async_stream::stream! {
+        for i in 0..1000 {
+            actix_web::rt::time::sleep(tick_duration).await;
+            yield Ok::<_, Error>(web::Bytes::from(format!("data: {}\n\n", i)));
+        }
+    };
+
+    HttpResponse::Ok()
+        .content_type("text/event-stream")
+        .streaming(body)
+}
+
+#[get("/stream")]
+async fn my_stream() -> HttpResponse {
+    let body = once(ok::<_, Error>(web::Bytes::from_static(b"test")));
+
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .streaming(body)
 }
 
 fn generic_error_handler<B>(
@@ -35,11 +64,15 @@ fn generic_error_handler<B>(
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(greet).wrap(
-            ErrorHandlers::new()
-                .handler(StatusCode::NOT_FOUND, generic_error_handler)
-                .handler(StatusCode::INTERNAL_SERVER_ERROR, generic_error_handler),
-        )
+        App::new()
+            .service(greet)
+            .service(my_stream)
+            .service(stream_delay)
+            .wrap(
+                ErrorHandlers::new()
+                    .handler(StatusCode::NOT_FOUND, generic_error_handler)
+                    .handler(StatusCode::INTERNAL_SERVER_ERROR, generic_error_handler),
+            )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
